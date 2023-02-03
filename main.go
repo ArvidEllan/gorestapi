56caeeec1f4d14431f502b610e930d39533f1ae5
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-03 11:53:08 +0300

    changes to correct endpoints

--- main.go
+++ main.go
@@ -7,41 +7,79 @@ import (
 	"net/http"
 )
 
-
 type Article struct {
-	
-	Title string 'json:"Title"'
-	Desc string 'json:"desc"'
-	Content string 'json:"content"'
-
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
 }
-//var Articles []Article
+
+var Articles []Article
 
 func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Article {
+	articles := []Article{
 		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
 	}
 
 	fmt.Println("Endpoint Hit: All articles Endpoint")
 	json.NewEncoder(w).Encode(articles)
-
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "homepage Endpoint Hit")
-
+	fmt.Fprint(w, "Homepage Endpoint Hit")
 }
 
 func handleRequests() {
-
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
-
 }
 
 func main() {
-
-
 	handleRequests()
 }
+
+//package main
+
+//import (
+//"encoding/json"
+//"fmt"
+//"log"
+//"net/http"
+//)
+
+//type Article struct {
+
+//Title string 'json:"Title"'
+//Desc string 'json:"desc"'
+//Content string 'json:"content"'
+
+//}
+//var Articles []Article
+
+//func allArticles(w http.ResponseWriter, r *http.Request) {
+//articles := Articles {
+//Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
+//}
+
+//	fmt.Println("Endpoint Hit: All articles Endpoint")
+//	json.NewEncoder(w).Encode(articles)
+
+//}
+
+//func homePage(w http.ResponseWriter, r *http.Request) {
+//fmt.Fprint(w, "homepage Endpoint Hit")
+
+//}
+
+//func handleRequests() {
+
+//	http.HandleFunc("/", homePage)
+//	http.HandleFunc("/articles", allArticles)
+//	log.Fatal(http.ListenAndServe(":8081", nil))
+
+//}
+
+//func main() {
+
+//	handleRequests()
+//}
