e9d45eaf9c4c86c906fcbec09f51d08bf46f6421
ArvidEllan <ellanwambugu6@gmail.com>
2023-02-06 09:30:30 +0300

    undefined error on mux reversal

--- main.go
+++ main.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-// Existing code from above
-func handleRequests() {
-	// creates a new instance of a mux router
-	myRouter := mux.NewRouter().StrictSlash(true)
-	// replace http.HandleFunc with myRouter.HandleFunc
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/all", returnAllArticles)
-	// finally, instead of passing in nil, we want
-	// to pass in our newly created router as the second
-	// argument
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
 }
+
+var Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := []Article{
+		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
+	}
+
+	fmt.Println("Endpoint Hit: All articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Homepage Endpoint Hit")
+}
+
+func handleRequests() {
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
+
+//func main() {
+//	handleRequests()
+//}
 
 func main() {
 	fmt.Println("Rest API v2.0 - Mux Routers")
@@ -29,3 +49,49 @@ func main() {
 	}
 	handleRequests()
 }
+
+//package main
+
+//import (
+//"encoding/json"
+//"fmt"
+//"log"
+//"net/http"
+//)
+
+//type Article struct {
+
+//Title string 'json:"Title"'
+//Desc string 'json:"desc"'
+//Content string 'json:"content"'
+
+//}
+//var Articles []Article
+
+//func allArticles(w http.ResponseWriter, r *http.Request) {
+//articles := Articles {
+//Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
+//}
+
+//	fmt.Println("Endpoint Hit: All articles Endpoint")
+//	json.NewEncoder(w).Encode(articles)
+
+//}
+
+//func homePage(w http.ResponseWriter, r *http.Request) {
+//fmt.Fprint(w, "homepage Endpoint Hit")
+
+//}
+
+//func handleRequests() {
+
+//	http.HandleFunc("/", homePage)
+//	http.HandleFunc("/articles", allArticles)
+//	log.Fatal(http.ListenAndServe(":8081", nil))
+
+//}
+
+//func main() {
+
+//	handleRequests()
+//}
